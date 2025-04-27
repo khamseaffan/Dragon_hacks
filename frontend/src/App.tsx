a936c36fb2d969d6ff25ea7bdb53eb7c851b7caa
[mabdulrehman08] <[muhammadabdulrehman513@gmail.com]>
2025-04-27 02:51:41 -0400

    landing_page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 // import Login from "./components/Login.tsx";
 // import Logout from "./components/Logout.tsx";
 // import UserProfile from "./components/UserProfile.tsx";
+// import Home from "./components/Home.tsx";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Home />
+//     </div>
+//   )
+// }
+
+// --------new code 
+import { useAuth0 } from "@auth0/auth0-react";
 import Home from "./components/Home.tsx";
+import LandingPage from "./components/LandingPage.tsx";
 
 export default function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <Home />
+      {isAuthenticated ? <Home /> : <LandingPage />}
     </div>
-  )
+  );
 }
